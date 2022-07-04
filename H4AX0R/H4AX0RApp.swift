7df6ba9cc490e2e27aa09f644a82c1927a0ca49c
Vladislav Vorobev <vlad_imbo1994@mail.ru>
2022-07-04 21:36:05 +0500

    Initial Commit

---
+++ H4AX0R/H4AX0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4AX0RApp.swift
+//  H4AX0R
+//
+//  Created by Владислав Воробьев on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4AX0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
